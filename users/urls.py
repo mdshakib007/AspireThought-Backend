24584ff6c543bd94f43c1b8ce3d2b0aa606bbf7f
MD Shakib Ahmed <shakibahmed.528874@gmail.com>
2025-02-07 09:54:26 +0600

    new feature: follow fav. topic

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from users.views import UserRegistrationAPIView, UserLoginAPIView, UserLogoutAPIView, activate, UserProfileUpdateAPIView, UserViewSet, BookmarkAPIView, RequestVerification
+from users.views import UserRegistrationAPIView, UserLoginAPIView, UserLogoutAPIView, activate, UserProfileUpdateAPIView, UserViewSet, BookmarkAPIView, RequestVerification, FollowingAPIView
 
 router = DefaultRouter()
 router.register("list", UserViewSet)
@@ -14,5 +14,7 @@ urlpatterns = [
     path('update/', UserProfileUpdateAPIView.as_view(), name='user-profile-update'),
     path('bookmark/add/', BookmarkAPIView.as_view(), name='add-bookmark'),
     path('bookmark/remove/', BookmarkAPIView.as_view(), name='remove-bookmark'),
+    path('following/topic/add/', FollowingAPIView.as_view(), name='add-following'),
+    path('following/topic/remove/', FollowingAPIView.as_view(), name='remove-following'),
     path('request-verification/', RequestVerification.as_view(), name='request-verification'),
 ]
